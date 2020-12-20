a2a5967cb58d893c46effca11b453dce7bc03a81
Ele <elemorelli@pm.me>
2020-12-20 03:57:09 -0300

    Form button improvement

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,25 @@
 const form = document.getElementById("contactForm");
+const nameInput = document.getElementById("nameInput");
+const emailInput = document.getElementById("emailInput");
+const messageInput = document.getElementById("messageInput");
+const submitButton = document.getElementById("submitButton");
 
 const onSubmitForm = (event) => {
   event.preventDefault();
-  const name = event.target.children.name.value;
-  const email = event.target.children.email.value;
-  const message = event.target.children.message.value;
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const message = messageInput.value;
   console.log({ name, email, message });
 };
+
+const toggleDisable = () => {
+  submitButton.disabled = !form.checkValidity();
+};
+
+toggleDisable();
+
+nameInput.addEventListener("change", toggleDisable);
+emailInput.addEventListener("change", toggleDisable);
+messageInput.addEventListener("change", toggleDisable);
 
 form.onsubmit = onSubmitForm;
